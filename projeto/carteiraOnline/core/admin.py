b7ce21b07bf0ebe05a359f859e8bf08afffe5e13
wisewizardofthestars <93286655+wisewizardofthestars@users.noreply.github.com>
2022-03-12 14:49:51 +0000

    h

--- projeto/carteiraOnline/core/admin.py
+++ projeto/carteiraOnline/core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# from .models import Question
+
+# Register your models here.
+
+# admin.site.register(Question)
+
+from .models import Author, Genre, Book, BookInstance
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+
