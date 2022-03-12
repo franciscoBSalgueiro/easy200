38a98ba11d05f4d8677c9a36b8df7a9579c5e2c4
wisewizardofthestars <93286655+wisewizardofthestars@users.noreply.github.com>
2022-03-12 15:05:44 +0000

    hj

--- projeto/carteiraOnline/carteiraOnline/settings.py
+++ projeto/carteiraOnline/carteiraOnline/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tailwind',
-    'carteira.apps.CarteiraConfig',
+    'core.apps.CoreConfig',
     'theme',
     'core'
 ]
